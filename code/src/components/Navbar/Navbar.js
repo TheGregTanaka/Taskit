852612e4b8f47e369ec826d379b34fd1ecda2191
maye7573 <maye7573@colorado.edu>
2021-03-05 15:15:54 -0700

    Added drop down menu to icon

--- code/src/components/Navbar/Navbar.js
+++ code/src/components/Navbar/Navbar.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { AppBar, Toolbar, Button, IconButton, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
+import MenuItem from "@material-ui/core/MenuItem"
+import { Menu } from '@material-ui/core';
+import { useState } from 'react';
 
 //AppBar sample code from https://material-ui.com/components/app-bar/
 const useStyles = makeStyles((theme) => ({
@@ -19,21 +22,37 @@ const useStyles = makeStyles((theme) => ({
 export default function Navbar({ loggedIn }) {
   const classes = useStyles();
   const log = loggedIn ? "Log Out" : "Login";
+  const [open, setOpen] = useState(false)
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClick = (e) =>{
+    setOpen(!open)
+    setAnchorEl(document.querySelector('header'))
+  }
+  const handleClose = () => {
+    setOpen(!open)
+    setAnchorEl(null)
+  }
+  
 
   return (
     <AppBar position="fixed">
       <Toolbar>
-        <IconButton 
-          edge="start" 
-          className={classes.menuButton} 
-          color="inherit"
-          aria-label="menu"
-        >
-          <MenuIcon />
+      <IconButton edge="false" className={classes.menuButton} color="inherit" aria-label="menu">
+          <MenuIcon onClick={(e)=>handleClick(e)}/>
         </IconButton>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+       
+      </Menu>
         <Typography variant="h6" className={classes.title}>TaskIt</Typography>
-
-
         <Button color="inherit"><a href="/company_profile">Company Profile</a></Button>
         <Button color="inherit"><a href="/createTask">Create Task</a></Button>
         <Button color="inherit"><a href="/feed">Feed</a></Button>
