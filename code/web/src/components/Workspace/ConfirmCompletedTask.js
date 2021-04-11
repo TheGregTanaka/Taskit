84636c9483c444188cf5e4f0544ca83566e46aea
srirachaBoi <62912895+srirachaBoi@users.noreply.github.com>
2021-04-11 13:01:46 -0600

    Added accordian to confirm complete taskk

--- code/web/src/components/Workspace/ConfirmCompletedTask.js
+++ code/web/src/components/Workspace/ConfirmCompletedTask.js
@@ -3,10 +3,22 @@ import DetailedTask from '../DetailedTask/DetailedTask';
 import { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 
+import Accordion from '@material-ui/core/Accordion';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Typography from "@material-ui/core/Typography";
+
+import NotificationImportantIcon from '@material-ui/icons/NotificationImportant';
+import './style.css';
 
 const ConfirmCompletedTask = () => {
     const [tasks, setTasks] = useState([]);
     const [err, setErr] = useState(false);
+    const [expanded, setExpanded] = useState(false);
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
     const mountedRef = useRef(true);
 
     const user = JSON.parse(localStorage.getItem('user'));
@@ -28,17 +40,23 @@ const ConfirmCompletedTask = () => {
     }, []);
 
     return (
-        <div>
+        <div style={{marginBottom:"1vh", marginLeft:'6%', marginRight:'6%'}}>
             {tasks.length != 0 &&
-            <div className="" style={{marginLeft:'7%', marginRight:'7%'}}>
-                <div className='row'>
-                    <hr/>
-                    <div className="row">
-                        <div className="col" style={{float:"left"}}>
-                            <h6>Confirmation Required</h6>
-                        </div>
-                    </div>
-
+            <Accordion
+                expanded={expanded === "panel1"}
+                onChange={handleChange("panel1")}
+                style={{backgroundColor:"#1d2026"}}
+            >
+                <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1bh-content"
+                id="panel1bh-header"
+                style={{backgroundColor:"white"}}
+                >
+                    <Typography>Confirmation Required</Typography>
+                    <NotificationImportantIcon className="flicker" style={{color:"red"}}/>
+                </AccordionSummary>
+                <AccordionDetails>
                     <div className="row">
                         {!err && tasks.slice(0).reverse().map((task) => (<DetailedTask key={task.id}
                                                                 workerID={task.workerID}
@@ -58,9 +76,8 @@ const ConfirmCompletedTask = () => {
                                                                 taskMode="confirm"
                                                             />))}
                     </div>
-                    <hr/>
-                </div>
-            </div>}
+                </AccordionDetails>
+            </Accordion>}
         </div>
     )
 }
